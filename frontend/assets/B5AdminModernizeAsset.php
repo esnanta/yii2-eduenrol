c6528c3d65f3528bf930971b3493e094090a048a
esnanta <ombakrinai@gmail.com>
2025-12-26 19:28:01 +0700

    update

--- frontend/assets/B5AdminModernizeAsset.php
+++ frontend/assets/B5AdminModernizeAsset.php
@@ -15,18 +15,20 @@ class B5AdminModernizeAsset extends AssetBundle
 
         'assets/vendor/fontawesome-free-5.15.4/css/all.css',
         'assets/vendor/icon-awesome/css/font-awesome.min.css',
-        
+
         //CSS Global Compulsory
         'assets/css/styles.min.css',
     ];
-  
+
     public $js = [
         //Global Compulsory
-        //'assets/libs/jquery/dist/jquery.min.js',// DISABLE FOR SELECT2
+        //'assets/libs/jquery/dist/jquery.min.js',
         'assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js',
         'assets/js/sidebarmenu.js',
         'assets/js/app.min.js',
-        'assets/libs/simplebar/dist/simplebar.js'
+        'assets/libs/apexcharts/dist/apexcharts.min.js',
+        'assets/libs/simplebar/dist/simplebar.js',
+        'assets/js/dashboard.js'
     ];
 
     public $depends = [
