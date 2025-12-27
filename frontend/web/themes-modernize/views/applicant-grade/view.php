ffa0fab389b41aa9237b7de718f129c2a84e8197
esnanta <ombakrinai@gmail.com>
2025-12-27 20:56:22 +0700

    update

--- frontend/web/themes-modernize/views/applicant-grade/view.php
+++ frontend/web/themes-modernize/views/applicant-grade/view.php
@@ -10,73 +10,80 @@ use kartik\detail\DetailView;
 
 $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Applicant Grades'),
-    'url' => ['index','sem'=>$model->semester_id]];
+        'url' => ['index', 'sem' => $model->semester_id]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="applicant-grade-view">
 
     <?= DetailView::widget([
-        'model' => $model,
-        'condensed' => false,
-        'hover' => true,
-        'mode' => Yii::$app->request->get('edit') == 't' ? DetailView::MODE_EDIT : DetailView::MODE_VIEW,
-        'panel' => [
-            'heading' => $this->title,
-            'type' => DetailView::TYPE_DEFAULT,
-        ],
-        'buttons1' => '{update}',
-        'attributes' => [
-            [
-                'attribute' => 'course_id',
-                'value' => ($model->course_id != null) ? $model->course->title : '',
-                'format' => 'html',
-                'type' => DetailView::INPUT_SELECT2,
-                'options' => ['id' => 'course_id', 'prompt' => '', 'disabled' => true],
-                'items' => $courseList,
-                'widgetOptions' => [
-                    'class' => Select2::class,
-                    'data' => $courseList,
-                ],
+            'model' => $model,
+            'condensed' => false,
+            'hover' => true,
+            'mode' => Yii::$app->request->get('edit') == 't' ? DetailView::MODE_EDIT : DetailView::MODE_VIEW,
+            'panel' => [
+                    'heading' => $this->title,
+                    'type' => DetailView::TYPE_DEFAULT,
             ],
-            [
-                'attribute' => 'semester_id',
-                'value' => ($model->semester_id != null) ? $model->semester->title : '',
-                'format' => 'html',
-                'type' => DetailView::INPUT_SELECT2,
-                'options' => ['id' => 'semester_id', 'prompt' => '', 'disabled' => true],
-                'items' => $semesterList,
-                'widgetOptions' => [
-                    'class' => Select2::class,
-                    'data' => $semesterList,
-                ],
-            ],
-            'grade',
-
-            [
-                'group'=>true,
-                'rowOptions'=>['class'=>'default']
-            ],
-            [
-                'columns' => [
+            'buttons1' => '{update}',
+            'attributes' => [
                     [
-                        'attribute' => 'created_at',
-                        'format' => 'date',
-                        'type' => DetailView::INPUT_HIDDEN,
-                        'valueColOptions' => ['style' => 'width:30%']
+                            'attribute' => 'course_id',
+                            'value' => ($model->course_id != null) ? $model->course->title : '',
+                            'format' => 'html',
+                            'type' => DetailView::INPUT_SELECT2,
+                            'options' => ['id' => 'course_id', 'prompt' => '', 'disabled' => true],
+                            'items' => $courseList,
+                            'widgetOptions' => [
+                                    'class' => Select2::class,
+                                    'data' => $courseList,
+                            ],
                     ],
                     [
-                        'attribute' => 'updated_at',
-                        'format' => 'date',
-                        'type' => DetailView::INPUT_HIDDEN,
-                        'valueColOptions' => ['style' => 'width:30%']
+                            'attribute' => 'semester_id',
+                            'value' => ($model->semester_id != null) ? $model->semester->title : '',
+                            'format' => 'html',
+                            'type' => DetailView::INPUT_SELECT2,
+                            'options' => ['id' => 'semester_id', 'prompt' => '', 'disabled' => true],
+                            'items' => $semesterList,
+                            'widgetOptions' => [
+                                    'class' => Select2::class,
+                                    'data' => $semesterList,
+                            ],
                     ],
-                ],
+                    'grade',
+                    [
+                            'label' => '',
+                            'format' => 'raw',
+                            'displayOnly' => true,
+                            'value' => '<span class="text-muted" style="font-size:12px;">
+                                            Gunakan titik (.) sebagai pemisah ribuan. Contoh: 89.54
+                                        </span>',
+                    ],
+                    [
+                            'group' => true,
+                            'rowOptions' => ['class' => 'default']
+                    ],
+                    [
+                            'columns' => [
+                                    [
+                                            'attribute' => 'created_at',
+                                            'format' => 'date',
+                                            'type' => DetailView::INPUT_HIDDEN,
+                                            'valueColOptions' => ['style' => 'width:30%']
+                                    ],
+                                    [
+                                            'attribute' => 'updated_at',
+                                            'format' => 'date',
+                                            'type' => DetailView::INPUT_HIDDEN,
+                                            'valueColOptions' => ['style' => 'width:30%']
+                                    ],
+                            ],
+                    ],
             ],
-        ],
-        'deleteOptions' => [
-            'url' => ['delete', 'id' => $model->id],
-        ],
-        'enableEditMode' => true,
+            'deleteOptions' => [
+                    'url' => ['delete', 'id' => $model->id],
+            ],
+            'enableEditMode' => true,
     ]) ?>
 
 </div>
